3d47d6e3d362a18f61f20345b8158c1ada57ed3e
Pécsi Tamás <44333929+akameleon@users.noreply.github.com>
2023-11-28 18:41:03 +0100

    softwareserial gps

--- sat/sat_lora/sat_lora.ino
+++ sat/sat_lora/sat_lora.ino
@@ -19,11 +19,10 @@ Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
 */
  
 #include <Adafruit_GPS.h>
-#include <HardwareSerial.h>
+#include <SoftwareSerial.h>
 
-#define GPSSerial Serial0
-
-Adafruit_GPS GPS(&GPSSerial);
+EspSoftwareSerial::UART locSerial;
+Adafruit_GPS GPS(&locSerial);
 
 unsigned long delayTime;
 
@@ -61,6 +60,7 @@ Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 sensors_event_t event; 
 
 void setup() {
+    locSerial.begin(9600, EspSoftwareSerial::SWSERIAL_8N1, 7, 8);
     
     //while(!Serial); // Várakozás a serial kapcsolat felépülésére
      
@@ -70,19 +70,20 @@ void setup() {
     
     //Serial.println("BME280 test");
 
-    unsigned status;
+    //unsigned status;
 
-    status = bme.begin();  
+    //status = 
+    bme.begin();  
 
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
-        while (1) delay(10);
-    }
+    //if (!status) {
+    //    Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+    //    Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+    //    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    //    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    //    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    //    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    //    while (1) delay(10);
+    //}
 
     /*
     * GPS initializáció
@@ -90,15 +91,22 @@ void setup() {
     
     //Serial.println("GPS test");
 
-    GPS.begin(9600);
+    if(locSerial){
 
-    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
-    GPS.sendCommand(PGCMD_ANTENNA);
+      GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+      GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+      GPS.sendCommand(PGCMD_ANTENNA);
 
-    delay(1000);
+      delay(1000);
   
-    GPSSerial.println(PMTK_Q_RELEASE);
+      locSerial.println(PMTK_Q_RELEASE);
+    } else {
+      while (true){
+        Serial.println("zsido");
+      }
+    }
+
+    
 
     if(!accel.begin())
   {
@@ -165,5 +173,5 @@ String gpsRead() {
   if (GPS.minute < 10) { mgpsdecimal = "0"; }
   if (GPS.seconds < 10) { sgpsdecimal = "0"; }
 
-  return String(GPS.latitudeDegrees, 5) + ", " + String(GPS.longitudeDegrees, 5) + "|" + String(GPS.satellites) + "|20" + String(GPS.year) + "-" + String(GPS.month) + "-" + String(GPS.day) + " " + String(GPS.hour) + String(gpsdecimal) + ":" + String(GPS.minute) + String(mgpsdecimal) + ":" + String(GPS.seconds) + String(sgpsdecimal) + "|" + String(GPS.angle);
+  return String(GPS.latitudeDegrees, 5) + ", " + String(GPS.longitudeDegrees, 5) + "|" + String(GPS.satellites) + "|20" + String(GPS.year) + "-" + String(GPS.month) + "-" + String(GPS.day) + " " + String(gpsdecimal) + String(GPS.hour) + ":" + String(mgpsdecimal) + String(GPS.minute) + ":" + String(sgpsdecimal) + String(GPS.seconds) + "|" + String(GPS.angle);
 }
