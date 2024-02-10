88a3b4ab7c85aa43d6142fa1cabefc4e38b39fcd
Robeszpier4 <148487611+Robeszpier4@users.noreply.github.com>
2024-02-10 16:04:03 +0100

    FFDFR

--- sat/sat_lora/sat_lora.ino
+++ sat/sat_lora/sat_lora.ino
@@ -1,18 +1,220 @@
 /*
+* SD kártya inicializáció
+*/
+
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.path(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void createDir(fs::FS &fs, const char * path){
+    Serial.printf("Creating Dir: %s\n", path);
+    if(fs.mkdir(path)){
+        Serial.println("Dir created");
+    } else {
+        Serial.println("mkdir failed");
+    }
+}
+
+void removeDir(fs::FS &fs, const char * path){
+    Serial.printf("Removing Dir: %s\n", path);
+    if(fs.rmdir(path)){
+        Serial.println("Dir removed");
+    } else {
+        Serial.println("rmdir failed");
+    }
+}
+
+String readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\n", path);
+    String adat = "ßß-NA$$-";
+    File file = fs.open(path);
+    if(!file){
+        Serial.println("Failed to open file for reading");
+        return "ßß-NA$$-";
+    }
+
+    adat = file.readString();
+    file.close();
+    return adat;
+
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+    file.close();
+}
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+    Serial.printf("Renaming file %s to %s\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("File renamed");
+    } else {
+        Serial.println("Rename failed");
+    }
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\n", path);
+    if(fs.remove(path)){
+        Serial.println("File deleted");
+    } else {
+        Serial.println("Delete failed");
+    }
+}
+
+void testFileIO(fs::FS &fs, const char * path){
+    File file = fs.open(path);
+    static uint8_t buf[512];
+    size_t len = 0;
+    uint32_t start = millis();
+    uint32_t end = start;
+    if(file){
+        len = file.size();
+        size_t flen = len;
+        start = millis();
+        while(len){
+            size_t toRead = len;
+            if(toRead > 512){
+                toRead = 512;
+            }
+            file.read(buf, toRead);
+            len -= toRead;
+        }
+        end = millis() - start;
+        Serial.printf("%u bytes read for %u ms\n", flen, end);
+        file.close();
+    } else {
+        Serial.println("Failed to open file for reading");
+    }
+
+
+    file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+
+    size_t i;
+    start = millis();
+    for(i=0; i<2048; i++){
+        file.write(buf, 512);
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+    file.close();
+}
+
+String lora_mod, lora_sf;
+int lora_freq, lora_pwr, lora_bw, lora_sync;
+
+void extractDataFromString(String data) {
+  // A String-et sorokra bontja a sortörés alapján
+  String lines[6]; // A sorokat tároló tömb
+  int index = 0;
+  int startPos = 0;
+  
+  while (index < 6 && startPos < data.length()) {
+    int endPos = data.indexOf('\n', startPos);
+    if (endPos == -1) {
+      endPos = data.length(); // Az utolsó sor
+    }
+
+    lines[index] = data.substring(startPos, endPos);
+    startPos = endPos + 1; // Az új sor kezdete
+    index++;
+  }
+
+  // Sorokból az értékek kinyerése
+  for (int i = 0; i < 6; i++) {
+    int equalsIndex = lines[i].indexOf('=');
+    if (equalsIndex != -1) {
+      String key = lines[i].substring(0, equalsIndex);
+      String value = lines[i].substring(equalsIndex + 1);
+
+      if (key.equals("lora_mod")) {
+        lora_mod = value;
+      } else if (key.equals("lora_freq")) {
+        lora_freq = value.toInt();
+      } else if (key.equals("lora_pwr")) {
+        lora_pwr = value.toInt();
+      } else if (key.equals("lora_sf")) {
+        lora_sf = value;
+      } else if (key.equals("lora_bw")) {
+        lora_bw = value.toInt();
+      } else if (key.equals("lora_sync")) {
+        lora_sync = value.toInt();
+      }
+    }
+  }
+}
+
+/*
 * Hőmérséklet+páratartalom+légnyomás (BME280) szenzor könyvtár importálás
 */
  
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-#define BME_SCK 12
-#define BME_MISO 11
-#define BME_MOSI 10
-#define BME_CS 9
-
-Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
+Adafruit_BME280 bme; // I2C
 
 /*
 * GPS szenzor könyvtár importálás
@@ -21,6 +223,7 @@ Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
 #include <Adafruit_GPS.h>
 #include <SoftwareSerial.h>
 
+EspSoftwareSerial::UART carbondioxideSerial;
 EspSoftwareSerial::UART locSerial;
 Adafruit_GPS GPS(&locSerial);
 
@@ -37,8 +240,6 @@ unsigned long delayTime;
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
-
-#define LED_BUILTIN 13
 
 float xInit = 0;
 float yInit = 0;
@@ -65,10 +266,36 @@ sensors_event_t event;
 
 void setup() {
     locSerial.begin(9600, EspSoftwareSerial::SWSERIAL_8N1, 7, 8);
+    carbondioxideSerial.begin(9600, EspSoftwareSerial::SWSERIAL_8N1, 5, 6);
     Serial0.begin(115200, SERIAL_8N1, 1, 0);
     Serial.begin(115200);
-    delay(1000);
-    
+    delay(5000);
+
+    Serial.print("Initializing SD card...");
+
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+
+  String settingsfiledata = readFile(SD, "/settings.csat");
+
+  if(settingsfiledata == "ßß-NA$$-"){
+    writeFile(SD, "/settings.csat", "lora_mod=lora\nlora_freq=868100000\nlora_pwr=1\nlora_sf=sf7\nlora_bw=125\nlora_sync=34");
+  }
+
+  extractDataFromString(settingsfiledata);
+  Serial0.println("radio set mod " + lora_mod);
+  sleep(1);
+  Serial0.println("radio set freq " + String(lora_freq));
+  sleep(1);
+  Serial0.println("radio set pwr " + String(lora_pwr));
+  sleep(1);
+  Serial0.println("radio set sf " + lora_sf);
+  sleep(1);
+  Serial0.println("radio set bw " + String(lora_bw));
+  sleep(1);
+  Serial0.println("radio set sync " + String(lora_sync));
     //while(!Serial); // Várakozás a serial kapcsolat felépülésére
      
     /*
@@ -140,14 +367,12 @@ void setup() {
   yHiba = yVart-(yInit/20);
   zHiba = zVart-(zInit/20);
   seaLevel = pInit/20;
-  
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 uint32_t timer = millis();
 
 void loop() { 
+
     String gps;
     char c = GPS.read();
 
@@ -155,13 +380,16 @@ void loop() {
       if (!GPS.parse(GPS.lastNMEA())) return;
     }
 
-    if (millis() - timer > 200) {
+    if (millis() - timer > 50) {
       timer = millis();
       //int sensorPin = A7;
       //int sensorValue = analogRead(sensorPin);
       //gps|gps_speed|gps_muhold|gps_time|gps_angle|temperature|humidity|pressure|calibrated_alt|x_real|y_real|z_real|x|y|z
       //if(Serial0.available()){
+        //Serial.println(carbondioxideSerial.read());
         Serial0.println("radio tx " + stringToHex(gpsRead() + "|" + bmeRead() + "|" + acceleroRead()) + " 1");
+        Serial.println("radio tx " + stringToHex(gpsRead() + "|" + bmeRead() + "|" + acceleroRead()) + " 1");
+
       //} else {
         //Serial.println("radio tx " + gpsRead() + "|" + bmeRead() + "|" + acceleroRead() + "|" + " 1");
         //Serial.println(Serial0.read());
@@ -203,4 +431,8 @@ String gpsRead() {
   if (GPS.seconds < 10) { sgpsdecimal = "0"; }
 
   return String(GPS.latitudeDegrees, 5) + ", " + String(GPS.longitudeDegrees, 5) + "|" + String(GPS.satellites) + "|20" + String(GPS.year) + "-" + String(GPS.month) + "-" + String(GPS.day) + " " + String(gpsdecimal) + String(GPS.hour) + ":" + String(mgpsdecimal) + String(GPS.minute) + ":" + String(sgpsdecimal) + String(GPS.seconds) + "|" + String(GPS.angle);
+}
+
+String methaneRead() {
+  return "0";
 }
